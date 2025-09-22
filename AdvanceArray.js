2205b7b2b67ac43cb0a0a0610594b7b4440955d7
Samiksha <samikshyabhandari592@gmail.com>
2025-09-22 13:54:54 +0545

    advance array

---
+++ AdvanceArray.js
@@ -0,0 +1,27 @@
+// const arr=[3,4,5,6,9,7,6,5,8,10];
+// console.log(arr.length);
+// console.log(arr.join("_"))
+// console.log(arr.at(3));
+// console.log(arr.pop())
+// console.log(arr.push(12));
+// console.log(arr.shift());
+// console.log(arr.unshift(2))
+ // console.log(arr.slice(1,5))
+// console.log(arr.splice(1,4,0,1))
+// console.log(arr);
+
+
+const fruits=["Apple","banana","orange","grapes","Kiwi"];
+console.log(fruits.length)
+console.log(fruits.join())
+console.log(fruits.at(3));
+console.log(fruits.pop())
+console.log(fruits.push("Mango"))
+console.log(fruits)
+console.log(fruits.shift())
+console.log(fruits.unshift("Avogardo"))
+console.log(fruits)
+console.log(fruits.slice(0,2))
+console.log(fruits)
+console.log(fruits.splice(1,4,"cucumber","Dragonfruit"))
+console.log(fruits)
